de1b18d833cad4f460361002fe43669e37edb056
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2026-06-25 23:05:18 -0300

    DEV: Update driver

--- backend/src/db/client.ts
+++ backend/src/db/client.ts
@@ -1,8 +1,16 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
 import pg from "pg";
 
-const pool = new pg.Pool({
-    connectionString: Deno.env.get("DATABASE_URL"),
-});
+const isDev = Deno.env.get("ENVIRONMENT") === "development";
+// o Deno.env.get("NODE_ENV") === "development"
 
-export const db = drizzle(pool);
+export const db = isDev
+    ? drizzlePg(
+        new pg.Pool({
+            connectionString: Deno.env.get("DATABASE_URL"),
+        })
+    )
+    : drizzle(neon(Deno.env.get("DATABASE_URL")!));
