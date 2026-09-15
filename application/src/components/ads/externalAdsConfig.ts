ca7ee741111b8e03d8ed2d7bc1cdbf02a539b415
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2026-09-15 02:15:02 -0300

    .

--- application/src/components/ads/externalAdsConfig.ts
+++ application/src/components/ads/externalAdsConfig.ts
@@ -54,4 +54,12 @@ export const externalAds: ExternalAdConfig[] = [
             params: {},
         },
     },
+    {
+        id: "monetag-push",
+        scriptSrc: "https://nap5k.com/tag.min.js",
+        containerId: "monetag-push",
+        placement: "explore_banner",
+        scriptAttrs: { "data-zone": "11805758" },
+        weight: 2,
+    }
 ];
